5bddc26b931c3a0f3be0d17f13c9a488c0d1024f
Zasimovich <o.zasimovich@gmail.com>
2023-10-14 23:52:46 +0000

    change file

--- Lesson2/init.sh
+++ Lesson2/init.sh
@@ -7,7 +7,10 @@ apt update
 apt install -y nginx
 
 # Configure Nginx
-echo "Hello, world! & Atlantis" > /var/www/html/index.html
+echo "<H1>Hello, world! & Atlantis </H1>" > /var/www/html/index.html
+IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+echo "<H2>My IP address is $IP </H2>" >> /var/www/html/index.html
+
 
 # Start Nginx service
 systemctl start nginx
